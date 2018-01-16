1c6985e893acc5e877d26ce6bec3b7deed9aadd8
Jonathan Norstrom <jon.norstrom@gmail.com>
2018-01-15 20:33:43 -0500

    add shoulda-matchers and write basic user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  string_columns = [:username, :password_digest]
+
+  string_columns.each do |col|
+    it { should have_db_column(col).of_type(:string).with_options(null: false) }
+    it { should validate_presence_of(col) }
+  end
 end
