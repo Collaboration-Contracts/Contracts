a7398dbbc08204c568fc40507be6fde0f7373d45
Sean Norton <Norton.Sean.M@gmail.com>
2018-01-25 22:36:36 -0600

    add username registration validation tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
       minimum: MIN_USERNAME_LENGTH,
       maximum: MAX_USERNAME_LENGTH
     }, uniqueness: true
-
+    validates_format_of :username, :with => /\A[A-Za-z0-9]*\z/
   has_secure_password
 end
--- features/step_definitions/registration_steps.rb
+++ features/step_definitions/registration_steps.rb
@@ -1,27 +1,25 @@
-When("I register with a username") do
+Given("I enter a username") do
   visit register_path
   fill_in 'username', with: "TheEdge"
-  click_button("Register")
 end
 
-Then("I have an account") do
-  expect(User.find_by(username: "TheEdge")).to be_truthy
-end
-
-When("I register with a username and with a password") do
-  visit register_path
-  fill_in 'username', with: "TheEdge"
+And("I enter a password") do
   fill_in 'password', with: "password"
+end
+
+When ("I register") do
   click_button("Register")
 end
 
-When("I register without a username and with a password") do
-  visit register_path
-  fill_in 'password', with: "password"
-  click_button("Register")
+Then ("I have an account") do
+  expect(User.count).to eq(1)
 end
 
-Then("I am not registered") do
+And ("I do not enter a password") do
+  fill_in 'password', with: ""
+end
+
+Then ("I do not have an account") do
   expect(User.find_by(username: "TheEdge")).to be_falsey
 end
 
@@ -29,8 +27,59 @@ And("I view the Registration form") do
   find("form#new_user")
 end
 
-When("I register with a username and without a password") do
+Given (/^I enter a valid (.*)$/) do |username|
   visit register_path
-  fill_in 'username', with: "TheEdge"
-  click_button("Register")
+  fill_in 'username', with: username
 end
+
+Given (/^I enter an invalid (.*)$/) do |username|
+  visit register_path
+  fill_in 'username', with: username
+end
+
+Then(/^I have an account as (.*)$/) do |username|
+    expect(User.find_by(username: username)).to be_truthy
+end
+
+Then(/^I do not have an account as (.*)$/) do |username|
+    expect(User.find_by(username: username)).to be_falsey
+end
+
+Given ("the username {string} exists") do |username|
+  User.create(username: username, password: 'password')
+end
+
+And ("I enter the username {string}") do |username|
+  visit register_path
+  fill_in 'username', with: username
+end
+
+
+
+#
+# For example, say you have this:
+#
+# ===============================
+#   Scenario: Test it out
+#   Given I do something to with
+#       | Setting | Value |
+#       | Some Setting | Some Value |
+# ===============================
+#
+# and you have a matcher like this
+#
+# ===============================
+# Given (/I do something to with$/) do |table|
+#   puts ("Table: #{table}")
+#   puts ("Table class: #{table.class}")
+# end
+# ===============================
+#
+# You will get this output:
+#
+# ===============================
+#    Given I do something to with
+#       Specifics:
+#         |     Setting      |     Value      |
+#         |     Some Setting |     Some Value |
+#       Specifics class: Cucumber::Ast::Table
