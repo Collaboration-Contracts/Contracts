3b421fd953ccfe55e214a082d98a985e61360cfa
Sean Norton <Norton.Sean.M@gmail.com>
2018-02-04 21:06:12 -0600

    add footing for tooltip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@ class HomeController < ApplicationController
   protect_from_forgery with: :exception
 
   def index
+    @page_title = "Collaboration Contracts"
+    render 'contracts/new'
   end
 
 end
---
+++ config/initializers/role_definitions.rb
@@ -0,0 +1,6 @@
+EXPLAIN = { title: 'Sole Decision Maker ',         action: 'Will explain my decision' }
+CONSULT = { title: 'Sole Decision Maker',         action: 'Will consult with other prior to making a decision' }
+AGREE =   { title: 'Group Decision Maker',        action: 'Will agree with other decision makers' }
+ADVISE =  { title: 'Not a Decision Maker ',        action: 'Will advise the decision maker(s)' }
+ABSTAIN = { title: 'Not a participant',           action: 'Will abstain from the process'}
+ACCEPT =  { title: 'Not a Decision Participant',  action: 'Will accept the decision' }
