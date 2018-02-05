d91e486b91fd5ca2549b8f6f88a58f06204faebf
Jonathan Norstrom <jon.norstrom@gmail.com>
2018-02-04 23:34:06 -0500

    styles main contract table outline

--- config/initializers/role_definitions.rb
+++ config/initializers/role_definitions.rb
@@ -1,6 +1,8 @@
-EXPLAIN = { title: 'Sole Decision Maker ',         action: 'Will explain my decision' }
-CONSULT = { title: 'Sole Decision Maker',         action: 'Will consult with other prior to making a decision' }
-AGREE =   { title: 'Group Decision Maker',        action: 'Will agree with other decision makers' }
-ADVISE =  { title: 'Not a Decision Maker ',        action: 'Will advise the decision maker(s)' }
-ABSTAIN = { title: 'Not a participant',           action: 'Will abstain from the process'}
-ACCEPT =  { title: 'Not a Decision Participant',  action: 'Will accept the decision' }
+ROLES = {
+  "Explain" => { title: 'Sole Decision Maker ',         action: 'Will explain my decision' },
+  "Consult" => { title: 'Sole Decision Maker',         action: 'Will consult with other prior to making a decision' },
+  "Agree" =>   { title: 'Group Decision Maker',        action: 'Will agree with other decision makers' },
+  "Advise" =>  { title: 'Not a Decision Maker ',        action: 'Will advise the decision maker(s)' },
+  "Abstain" => { title: 'Not a participant',           action: 'Will abstain from the process'},
+  "Accept" =>  { title: 'Not a Decision Participant',  action: 'Will accept the decision'}
+}
