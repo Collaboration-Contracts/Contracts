daaf96a3777a6f9f583289b45fd6c4747f153c09
Doc Norton <michael@docondev.com>
2018-02-10 17:13:34 -0500

    Changes text to match comp. Update k/v reference in code for readability.

--- config/initializers/role_definitions.rb
+++ config/initializers/role_definitions.rb
@@ -1,8 +1,8 @@
 ROLES = {
   "Explain" => { title: 'Sole Decision Maker ',         action: 'Will explain my decision' },
-  "Consult" => { title: 'Sole Decision Maker',         action: 'Will consult with other prior to making a decision' },
+  "Consult" => { title: 'Sole Decision Maker',         action: 'Will consult with others prior to making a decision' },
   "Agree" =>   { title: 'Group Decision Maker',        action: 'Will agree with other decision makers' },
   "Advise" =>  { title: 'Not a Decision Maker ',        action: 'Will advise the decision maker(s)' },
   "Accept" =>  { title: 'Not a Decision Participant',  action: 'Will accept the decision'},
-  "Abstain" => { title: 'Not a participant',           action: 'Will abstain from the process'}
+  "Abstain" => { title: 'Not a Participant',           action: 'Will abstain from the process'}
 }
